300a53d5de39c173c79fb98e67b4aae3fd85e902
99Muhammad <MuhammadIbraheem99m@gmail.com>
2024-07-22 16:03:59 +0300

    ad

--- Magic-Land-Explorer_Lab/Program.cs
+++ Magic-Land-Explorer_Lab/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Magic_Land_Explorer_Lab
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Magic_Land_Explorer_Lab;
+using Newtonsoft.Json;
+
+namespace MagicLandExplorer
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            clsCategory category = new clsCategory();
+            clsMainScreen mainScreen = new clsMainScreen();
+            do
+            {
+                clsMainScreen.MainScreen();
+                clsMainScreen.ImplementUserChoise(mainScreen.ReadUserInput());
+
+               
+            } while (clsMainScreen.isExit== false);
+
+
         }
     }
 }
---
+++ Magic-Land-Explorer_Lab/clsDestination .cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic_Land_Explorer_Lab
+{
+
+        public class clsDestination
+        {
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public string Location { get; set; }
+            public string Duration { get; set; }
+            public string Description { get; set; }
+        }
+
+
+}
